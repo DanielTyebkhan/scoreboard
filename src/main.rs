9fdf2b37e1fdb2bd2905b4e28a1ce76884c78f82
Daniel Tyebkhan <dtyebkhan@gmail.com>
2025-09-05 02:03:10 +0000

    add basic server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+use std::net::SocketAddr;
+use tokio::net::TcpListener;
+
+async fn health_check() -> &'static str {
+    "API running"
+}
+
+#[tokio::main]
+async fn main() {
+    let router = Router::new().route("/health", get(health_check));
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
+    let tcp = TcpListener::bind(&addr).await.unwrap();
+    axum::serve(tcp, router).await.unwrap();
 }
